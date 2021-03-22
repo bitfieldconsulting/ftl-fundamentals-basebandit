00130e23d4b4adfb6ae203db9d8e5da317915ec5
basebandit <evansonmwangi83@gmail.com>
2021-03-22 16:09:34 +0300

    refactor and extend the existing tests to accept two or more inputs

--- calculator_test.go
+++ calculator_test.go
@@ -18,16 +18,16 @@ func TestAdd(t *testing.T) {
 	t.Parallel()
 
 	tests := []testCase{
-		{name: "Two positive fractional numbers which sum to positive", inputs: []float64{3.0, 2.0}, output: 5.0},
-		{name: "Two positive fractional numbers which sum to positive", inputs: []float64{0.50, 0.2}, output: 0.70},
-		{name: "Two positive numbers which sum to positive", inputs: []float64{200, 30}, output: 230},
-		{name: "Two positive fractional numbers which sum to positive", inputs: []float64{4.56, 3.56}, output: 8.12},
-		{name: "Two negative numbers which sum to positive", inputs: []float64{-2.0, -3.0}, output: -5.0},
-		{name: "One positive and one negative number which sum to negative", inputs: []float64{-5.0, 2.0}, output: -3.0},
+		{name: "Two positive fractional numbers which sum to positive", inputs: []float64{3.0, 2.0, 1.0}, output: 6.0},
+		{name: "Two positive fractional numbers which sum to positive", inputs: []float64{0.50, 0.2, 0.3}, output: 1.0},
+		{name: "Two positive numbers which sum to positive", inputs: []float64{200, 30, 50}, output: 280},
+		{name: "Three positive fractional numbers which sum to positive", inputs: []float64{4.56, 3.56, 1.06}, output: 9.18},
+		{name: "Two negative fractional numbers which sum to positive", inputs: []float64{-2.0, -3.0, -2.0}, output: -7.0},
+		{name: "One negative and two positive numbers which sum to negative", inputs: []float64{-5.0, 2.0, 0.0}, output: -3.0},
 	}
 
 	for _, tt := range tests {
-		got := calculator.Add(tt.inputs[0], tt.inputs[1])
+		got := calculator.Add(tt.inputs[0], tt.inputs[1], tt.inputs[2])
 		if tt.output != got {
 			t.Errorf("%s:\n\t\t\twant %f, got %f", tt.name, tt.output, got)
 		}
@@ -38,16 +38,16 @@ func TestSubtract(t *testing.T) {
 	t.Parallel()
 
 	tests := []testCase{
-		{name: "One negative number and one positive number which subtract to negative", inputs: []float64{-3.0, 2.0}, output: -5.0},
-		{name: "Two negative numbers which subtract to negative", inputs: []float64{-0.50, -0.2}, output: -0.3},
-		{name: "Two positive numbers which subtract to positive", inputs: []float64{200, 30}, output: 170},
-		{name: "One positive number and one negative number which subtract to positive", inputs: []float64{0.56, -3.56}, output: 4.12},
-		{name: "One positive number and one negative number which subtract to negative", inputs: []float64{0.0, -0.0}, output: 0.0},
-		{inputs: []float64{0, 0}, output: -1},
+		{name: "One negative fractional number and two positive fractional numbers which subtract to negative", inputs: []float64{-3.0, 2.0, 0.1}, output: -5.1},
+		{name: "Three negative fractional numbers which subtract to negative", inputs: []float64{-0.50, -0.2, -0.1}, output: -0.19999999999999998},
+		{name: "Three positive numbers which subtract to positive", inputs: []float64{200, 30, 50}, output: 120},
+		{name: "One positive fractional number and two negative fractional numbers which subtract to positive", inputs: []float64{0.56, -3.56, -0.2}, output: 4.32},
+		{name: "One positive fractional number and two fractional negative numbers which subtract to negative", inputs: []float64{0.0, -0.0, -0.0}, output: 0.0},
+		{inputs: []float64{0, 0, 0}, output: 0},
 	}
 
 	for _, tt := range tests {
-		got := calculator.Subtract(tt.inputs[0], tt.inputs[1])
+		got := calculator.Subtract(tt.inputs[0], tt.inputs[1], tt.inputs[2])
 		if tt.output != got {
 			t.Errorf("%s:\n\t\t\twant %f, got %f", tt.name, tt.output, got)
 		}
@@ -57,16 +57,16 @@ func TestSubtract(t *testing.T) {
 func TestMultiply(t *testing.T) {
 	t.Parallel()
 	tests := []testCase{
-		{name: "One negative number and one positive number which multiply to negative", inputs: []float64{-3.0, 2.0}, output: -6.0},
-		{name: "Two negative numbers which multiply to positive", inputs: []float64{-0.50, -0.2}, output: 0.1},
-		{name: "Two positive numbers which multiply to positive", inputs: []float64{200, 30}, output: 6000},
-		{name: "One positive number and one negative number which multiply to negative", inputs: []float64{0.56, -3.56}, output: -1.9936000000000003},
-		{name: "One positive number and one negative number which multiply to negative", inputs: []float64{0.0, -0.0}, output: -0.0},
-		{inputs: []float64{0, 0}, output: 1},
+		{name: "Two negative fractional number and one positive fractional number which multiply to negative", inputs: []float64{-3.0, 2.0, -1.0}, output: 6.0},
+		{name: "Two negative fractional numbers and one positive fractional number which multiply to positive", inputs: []float64{-0.50, -0.2, 0.1}, output: 0.010000000000000002},
+		{name: "Three positive numbers which multiply to positive", inputs: []float64{200, 30, 10}, output: 60000},
+		{name: "One positive fractional number and two negative fractional numbers which multiply to negative", inputs: []float64{0.56, -3.56, -0.1}, output: 0.19936000000000004},
+		{name: "One positive fractional number and one negative fractional number which multiply to negative", inputs: []float64{0.0, -0.0, -0.0}, output: 0.0},
+		{inputs: []float64{0, 0, 0}, output: 0},
 	}
 
 	for _, tt := range tests {
-		got := calculator.Multiply(tt.inputs[0], tt.inputs[1])
+		got := calculator.Multiply(tt.inputs[0], tt.inputs[1], tt.inputs[2])
 		if tt.output != got {
 			t.Errorf("%s:\n\t\t\twant %f,got %f", tt.name, tt.output, got)
 		}
@@ -79,18 +79,18 @@ func TestDivide(t *testing.T) {
 		output      float64
 		errExpected bool
 	}{
-		{name: "One positive number and one negative number which divide to negative", inputs: []float64{1, -1}, output: -1.0, errExpected: false},
-		{name: "Two positive numbers which divide to positive", inputs: []float64{4, 2}, output: 3, errExpected: false},
-		{name: "Two positive numbers which do not divide", inputs: []float64{6, 0}, errExpected: true},
-
-		{name: "Two negative numbers which divide to negative", inputs: []float64{-3, -5}, output: 0.6, errExpected: false},
+		{name: "One positive number and two negative numbers which divide to negative", inputs: []float64{1, -1, -1}, output: 1.0, errExpected: false},
+		{name: "Three positive numbers which divide to positive", inputs: []float64{4, 2, 1}, output: 2, errExpected: false},
+		{name: "Three positive numbers which do not divide", inputs: []float64{6, 0, 0}, errExpected: true},
+		{name: "Three positive numbers which do not divide", inputs: []float64{6, 0, 1}, errExpected: true},
+		{name: "Two negative numbers which divide to negative", inputs: []float64{-3, -5, -2}, output: -0.3, errExpected: false},
 	}
 
 	for _, tt := range tests {
-		got, err := calculator.Divide(tt.inputs[0], tt.inputs[1])
-		if tt.errExpected { //we expect an error
-			if err != nil { //therefore err must be non-nil(runtime error)
-				t.Errorf("%s: %v\n", tt.name, err)
+		got, err := calculator.Divide(tt.inputs[0], tt.inputs[1], tt.inputs[2])
+		if tt.errExpected {
+			if err == nil {
+				t.Errorf("%s:\n\t\t\twant error got %v", tt.name, err)
 			}
 		}
 
@@ -105,8 +105,9 @@ func TestAddRandom(t *testing.T) {
 	for i := 0; i < 100; i++ {
 		a := rand.Float64()
 		b := rand.Float64()
-		want := a + b
-		got := calculator.Add(a, b)
+		c := rand.Float64()
+		want := a + b + c
+		got := calculator.Add(a, b, c)
 		if want != got {
 			t.Errorf("want %f got %f", want, got)
 		}
