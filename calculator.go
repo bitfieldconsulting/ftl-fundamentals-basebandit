67d671a66fe6a36cea2afd498127039adf6b7717
basebandit <evansonmwangi83@gmail.com>
2021-03-22 17:05:02 +0300

    implement calc string function

--- calculator.go
+++ calculator.go
@@ -4,6 +4,8 @@ package calculator
 import (
 	"errors"
 	"math"
+	"strconv"
+	"strings"
 )
 
 // Add takes two numbers and returns the result of adding them together.
@@ -37,4 +39,50 @@ func Sqrt(a float64) (float64, error) {
 		return 0.0, errors.New("sqrt: negative numbers don't have real square roots")
 	}
 	return math.Sqrt(a), nil
+}
+
+func CalcString(expr string) (float64, error) {
+
+	operators := map[string]string{"multiplication": "*", "division": "/", "addition": "+", "subtraction": "-"}
+
+	var operator string
+
+	for k, v := range operators {
+		if strings.Contains(strings.TrimSpace(expr), v) {
+			operator = k
+			break
+		}
+	}
+
+	x := strings.Split(expr, operators[operator])[0]
+	operA, err := strconv.ParseFloat(x, 64)
+
+	if err != nil {
+		panic(err)
+	}
+
+	y := strings.Split(expr, operators[operator])[1]
+	operB, err := strconv.ParseFloat(y, 64)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var result float64
+
+	switch operator {
+	case "multiplication":
+		result = operA * operB
+	case "division":
+		result = operA / operB
+	case "subtraction":
+		result = operA - operB
+	case "addition":
+		result = operA + operB
+
+	default:
+		return result, errors.New("calcstring: unknown operand")
+	}
+
+	return result, nil
 }
